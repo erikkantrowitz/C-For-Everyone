526be1856016f0e35078afacc6468e93d33b0df3
Erik Kantrowitz <erikkantrowitz@Eriks-MacBook-Pro.local>
2025-04-03 21:54:57 -0600

    Week 4 start

--- w2/sine.c
+++ w2/sine.c
@@ -4,7 +4,7 @@
 int main(void){
     double input;  //set type for input variable
     printf("Input value you would like to see sine of: ");
-    scanf("%lf", &input); //ask user for input, could have been integer but the problem didn't state limits on inout
+    scanf("%lf", &input); //ask user for input, using double for decimal reasons
     double sine =  sin(input);  //this is the math part that finds the sine of the inputed value
     printf("sine of %lf is: %lf",input,  sine); //print the result
     return 0; //end the program
